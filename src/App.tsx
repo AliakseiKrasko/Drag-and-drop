6cfd7ff7dd81f8a89f33a492f7c0846cac736087
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-14 20:06:05 +0100

    finished drag and drop cards

--- src/App.tsx
+++ src/App.tsx
@@ -10,40 +10,65 @@ type CardType = {
 function App() {
 
 
-    const [cardList] = useState<CardType[]>([
+    const [cardList, setCardList] = useState<CardType[]>([
         {id: 1, order: 3, text: 'Карточка 3'},
         {id: 2, order: 1, text: 'Карточка 1'},
         {id: 3, order: 2, text: 'Карточка 2'},
         {id: 4, order: 4, text: 'Карточка 4'}
     ])
 
+    const [currentCard, setCurrentCard] = useState<CardType | null>(null)
+
 
     function DragStartHandler(_e: React.DragEvent<HTMLDivElement>, card: CardType) {
-        console.log('drag', card)
+        setCurrentCard(card)
     }
 
 
-    function DragEndHandler() {
-
+    function DragEndHandler(e: React.DragEvent<HTMLDivElement>) {
+        e.currentTarget.style.background = 'white'
     }
 
     function DragOverHandler(e: React.DragEvent<HTMLDivElement>) {
         e.preventDefault()
+        const target = e.target as HTMLDivElement
+        target.style.background = 'lightgray'
     }
 
     function DropHandler(e: React.DragEvent<HTMLDivElement>, card: CardType) {
         e.preventDefault()
-        console.log('drop', card)
+
+        if (!currentCard) return // safety check
+
+        setCardList(cardList.map(c => {
+            if (c.id === card.id) {
+                return {...c, order: currentCard.order}
+            }
+            if (c.id === currentCard.id) {
+                return {...c, order: card.order}
+            }
+            return c
+        }))
+
+        e.currentTarget.style.background = 'white'
+    }
+
+    const sortCards = (a: CardType, b: CardType) => {
+        if(a.order > b.order){
+            return 1
+        } else {
+            return -1
+        }
     }
 
 
     return (
         <div className={'app'}>
-            {cardList.map((card) =>
+            {cardList.sort(sortCards).map((card) =>
                 <div
                     onDragStart={(e) => DragStartHandler(e, card)}
-                    onDragLeave={() => DragEndHandler()}
-                    onDragEnd={() => DragEndHandler()}
+                    onDragLeave={(e) => DragEndHandler(e)}
+                    onDragEnd={(e) => DragEndHandler(e)}
                     onDragOver={(e) => DragOverHandler(e)}
                     onDrop={(e) => DropHandler(e, card)}
                     key={card.id}
