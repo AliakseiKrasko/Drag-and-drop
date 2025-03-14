041aba1a4c7801a58d8e24f87bb1ec2f8960d4a6
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-14 19:13:53 +0100

    added useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cardList] = useState([
+      {id: 1, order: 3, text: 'Карточка 3'},
+      {id: 2, order: 1, text: 'Карточка 1'},
+      {id: 3, order: 2, text: 'Карточка 2'},
+      {id: 4, order: 4, text: 'Карточка 4'}
+  ])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={'app'}>
+        {cardList.map((card) =>
+            <div key={'id'} className={'card'}>
+                {card.text}
+            </div>
+        )}
+    </div>
   )
 }
 
