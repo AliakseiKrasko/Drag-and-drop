83c9d88ecf473603e7fe55843a6d526b9b74480d
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-14 19:43:58 +0100

    added onDragStart and all hundlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
 import {useState} from 'react'
 import './App.css'
 
+type CardType = {
+    id: number
+    order: number
+    text: string
+}
+
 function App() {
-  const [cardList] = useState([
-      {id: 1, order: 3, text: 'Карточка 3'},
-      {id: 2, order: 1, text: 'Карточка 1'},
-      {id: 3, order: 2, text: 'Карточка 2'},
-      {id: 4, order: 4, text: 'Карточка 4'}
-  ])
 
 
-  return (
-    <div className={'app'}>
-        {cardList.map((card) =>
-            <div
-                key={card.id}
-                draggable={true}
-                className={'card'}>
-                {card.text}
-            </div>
-        )}
-    </div>
-  )
+    const [cardList] = useState<CardType[]>([
+        {id: 1, order: 3, text: 'Карточка 3'},
+        {id: 2, order: 1, text: 'Карточка 1'},
+        {id: 3, order: 2, text: 'Карточка 2'},
+        {id: 4, order: 4, text: 'Карточка 4'}
+    ])
+
+
+    function DragStartHandler(_e: React.DragEvent<HTMLDivElement>, card: CardType) {
+        console.log('drag', card)
+    }
+
+
+    function DragEndHandler() {
+
+    }
+
+    function DragOverHandler(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+    }
+
+    function DropHandler(e: React.DragEvent<HTMLDivElement>, card: CardType) {
+        e.preventDefault()
+        console.log('drop', card)
+    }
+
+
+    return (
+        <div className={'app'}>
+            {cardList.map((card) =>
+                <div
+                    onDragStart={(e) => DragStartHandler(e, card)}
+                    onDragLeave={() => DragEndHandler()}
+                    onDragEnd={() => DragEndHandler()}
+                    onDragOver={(e) => DragOverHandler(e)}
+                    onDrop={(e) => DropHandler(e, card)}
+                    key={card.id}
+                    draggable={true}
+                    className={'card'}>
+                    {card.text}
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default App
